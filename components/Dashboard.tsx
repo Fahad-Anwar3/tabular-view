9af2f6e0c835b4e2b987a36596e5b488dda918c0
subhan-sikandar <subhan.sikandar@zweidevs.com>
2025-05-05 00:06:51 +0500

    update routes

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -3,15 +3,19 @@
 import Header from "./Header"
 import StockTable from "./StockTable"
 import Footer from "./Footer"
+import { Topbar } from "./topbar"
 
 export default function Dashboard() {
   return (
-    <div className="flex flex-col min-h-screen bg-dashboard-background">
+    <>
+    <Topbar />
+      <div className="flex flex-col min-h-screen bg-dashboard-background">
       <Header />
-      <main className="flex-1">
+      <main className="flex-1 overflow-x-auto">
         <StockTable />
       </main>
       <Footer />
     </div>
+    </>
   )
 }
